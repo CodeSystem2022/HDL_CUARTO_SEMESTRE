7cc090514f2c59dd8f58d0f15d5e4a429661fe49
SantiagoCascón <112910326+SantiagoCascon@users.noreply.github.com>
2023-09-20 20:02:05 -0300

    Clase 05 Python
    Trabajamos con cadenas, métodos join y split

---
+++ PYTHON/Clase01/mi_clase.py
@@ -0,0 +1,19 @@
+
+class MiClase:
+    '''
+    Este es un ejemplo de documentacion de esta clase
+    '''
+    def __init__(self):
+        '''
+        Aquie esta el metodo dunder init
+        este metodo es el de inicio para esta clase
+        '''
+    def mi_metodo(self, param1, param2):
+        """
+        Metodo creado por mi que resive parametros 
+        Detecto los parametros atuomaticamente
+        :param param1: Parametro 1
+        :param param2: Parametro 2
+        :return: Este es el valor del retorno
+        """
+
